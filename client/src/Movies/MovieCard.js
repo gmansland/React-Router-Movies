e8c2654a4b3a1a698d08a7436a82c4de18d0ea19
Sean Gauer <gmansland300@outlook.com>
2020-05-18 19:32:22 -0600

    push1 not done

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
 const MovieCard = props => {
-  return;
+  return (
+    <Link to={'/movies/${movie.id}'} />
+  )
 };
 
 export default MovieCard;
